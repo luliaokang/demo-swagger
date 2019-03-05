ce4e5d3ae32a10330fa34c4df31e36c6f4439c64
luliaokang <236104360@qq.com>
2019-03-05 17:58:35 +0800

    Update TestController.java

--- src/main/java/com/cgs/demo/swagger/controller/TestController.java
+++ src/main/java/com/cgs/demo/swagger/controller/TestController.java
@@ -19,7 +19,6 @@ public class TestController {
 	public String hello(
 			@ApiParam(required = false, value = "姓名") @RequestParam(required = false) String name) {
 		return "hello," + name;
-		
 	}
 	
 	@RequestMapping(value = "/hello2", method = RequestMethod.GET)
